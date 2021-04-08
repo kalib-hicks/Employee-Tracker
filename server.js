6eb4408e255dadfc154a30825532a572cc26f459
Kalib Hicks <kalib.hicks@gmail.com>
2021-04-08 19:40:19 -0400

    added question prompts to user

--- server.js
+++ server.js
@@ -18,3 +18,59 @@ const connection = mysql.createConnection({
     console.log("Connected as Id" + connection.threadId)
     startPrompt();
 });
+
+// start question prompts
+function startPrompt() {
+    inquirer.prompt([
+    {
+    type: "list",
+    message: "What would you like to do?",
+    name: "choice",
+    choices: [
+              "View All Employees", 
+              "View All Departments",
+              "View All Roles",
+              "View all Employees By Deparments", 
+              "Update Employee",
+              "Add Employee",
+              "Add Role",
+              "Add Department"
+            ]
+    }
+]).then(function(val) {
+        switch (val.choice) {
+            case "View All Employees":
+              viewAllEmployees();
+            break;
+
+            case "View All Departments":
+              viewAllDepts();
+            break;
+    
+          case "View All Roles":
+              viewAllRoles();
+            break;
+
+          case "View all Employees By Deparments":
+              viewAllDepartments();
+            break;
+          
+          case "Add Employee":
+                addEmployee();
+              break;
+
+          case "Update Employee":
+                updateEmployee();
+              break;
+      
+            case "Add Role":
+                addRole();
+              break;
+      
+            case "Add Department":
+                addDepartment();
+              break;
+    
+            }
+    })
+}
