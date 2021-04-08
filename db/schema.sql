ff0fc4e58288dd813e393e8e411fd88c9d7a0702
Kalib Hicks <kalib.hicks@gmail.com>
2021-04-08 19:21:08 -0400

    role and employee creation tables

--- db/schema.sql
+++ db/schema.sql
@@ -2,6 +2,35 @@
 
 DROP DATABASE IF EXISTS employees_db;
 
+/* create databse */
 CREATE database employees_db;
 
 USE employees_db;
+
+CREATE TABLE department (
+    id INT NOT NULL AUTO_INCREMENT,
+    name VARCHAR(30),
+    PRIMARY KEY (id)
+);
+
+/* role creation table */
+CREATE TABLE role (
+	id INT NOT NULL AUTO_INCREMENT,
+    title VARCHAR(30),
+    salary DECIMAL (8,2),
+    department_id INT,
+    PRIMARY KEY (id),
+    FOREIGN KEY (department_id) references department(id)
+);
+
+/*employee creation table*/
+CREATE TABLE employee (
+	id INT NOT NULL AUTO_INCREMENT,
+    first_name VARCHAR(30) NOT NULL,
+    last_name VARCHAR(30) NOT NULL,
+    role_id INT,
+    manager_id INT,
+    PRIMARY KEY (id),
+    FOREIGN KEY (role_id) REFERENCES role(id),
+    FOREIGN KEY (manager_id) REFERENCES employee(id)
+);
