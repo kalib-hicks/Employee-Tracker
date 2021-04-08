cfded3ca04f433032d1596672eb3d6a5aab9c5d9
Kalib Hicks <kalib.hicks@gmail.com>
2021-04-08 19:35:37 -0400

    added employee variables and gitignore

--- db/seeds.sql
+++ db/seeds.sql
@@ -7,3 +7,10 @@ INSERT INTO department (name) VALUES ("Human Resources");
 INSERT INTO department (name) VALUES ("SM Marketing");
 INSERT INTO department (name) VALUES ("Sales");
 INSERT INTO department (name) VALUES ("Corporate");
+
+/* employee variables*/
+INSERT INTO employee (first_name, last_name, role_id) VALUES ("Steve", "Rogers", 2);
+INSERT INTO employee (first_name, last_name, role_id) VALUES ("Natasha", "Romanoff", 1);
+INSERT INTO employee (first_name, last_name, role_id) VALUES ("Wanda", "Maximoff", 3);
+INSERT INTO employee (first_name, last_name, role_id) VALUES ("James", "Barnes", 4);
+INSERT INTO employee (first_name, last_name, role_id) VALUES ("Tony", "Stark", 5);
