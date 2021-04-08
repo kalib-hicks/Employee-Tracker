0a0a4c265b8aece88246b0fdf26ee4a1592d2579
Kalib Hicks <kalib.hicks@gmail.com>
2021-04-08 19:34:09 -0400

    added role variables

---
+++ db/seeds.sql
@@ -0,0 +1,9 @@
+/* use db */
+USE employees_db;
+
+
+/* role variables */
+INSERT INTO department (name) VALUES ("Human Resources");
+INSERT INTO department (name) VALUES ("SM Marketing");
+INSERT INTO department (name) VALUES ("Sales");
+INSERT INTO department (name) VALUES ("Corporate");
