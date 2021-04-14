27f6e80edae3549cc05e5da34e83a9e915ea1713
Kalib Hicks <kalib.hicks@gmail.com>
2021-04-13 22:43:52 -0400

    seeds update

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,14 +1,16 @@
-/* use db */
 USE employees_db;
 
-
-/* role variables */
 INSERT INTO department (name) VALUES ("Human Resources");
 INSERT INTO department (name) VALUES ("SM Marketing");
 INSERT INTO department (name) VALUES ("Sales");
 INSERT INTO department (name) VALUES ("Corporate");
 
-/* employee variables*/
+INSERT INTO role (title, salary, department_id) VALUES ("Sales Rep", 70000.00, 3);
+INSERT INTO role (title, salary, department_id) VALUES ("Communications Associate", 50000.00, 2);
+INSERT INTO role (title, salary, department_id) VALUES ("Social Media Manager", 50000.00, 2);
+INSERT INTO role (title, salary, department_id) VALUES ("Director", 100000.00, 1);
+INSERT INTO role (title, salary, department_id) VALUES ("Director", 100000.00, 4);
+
 INSERT INTO employee (first_name, last_name, role_id) VALUES ("Steve", "Rogers", 2);
 INSERT INTO employee (first_name, last_name, role_id) VALUES ("Natasha", "Romanoff", 1);
 INSERT INTO employee (first_name, last_name, role_id) VALUES ("Wanda", "Maximoff", 3);
